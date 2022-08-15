90c049a1ab0b8eb7adb89eafb1e9990bee3efeba
Luis Rodriguez <luisrc7@yahoo.es>
2022-08-15 15:43:04 +0100

    new endpoint

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,20 @@ export const handler = async (event: APIGatewayEvent, context: Context): Promise
   console.log(`Event: ${JSON.stringify(event, null, 2)}`);
   console.log(`Context: ${JSON.stringify(context, null, 2)}`);
   console.log(`User Agent: ${JSON.stringify(event.requestContext.identity.userAgent, null, 2)}`);
+  
+  if (JSON.stringify(event.path, null, 2) == 'my-endpoint') {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        msg: 'Response from my endpoint!',
+      }),
+   };
+  }
+  
   return {
       statusCode: 200,
       body: JSON.stringify({
-        msg: 'hello world',
+        msg: 'Hello World!!!',
       }),
    };
 };
